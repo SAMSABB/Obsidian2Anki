d3087f28b527baf914e6dab2eb3e265cc822a9ce
N1264172 <4bbott.sam@gmail.com>
2025-02-09 21:15:34 +0000

    TEST: Created and tested function which uses line splitting to parse the question and answer pairs.

--- Source/converter.py
+++ Source/converter.py
@@ -26,11 +26,60 @@ def read_obsidian_notes(notes_dir: str)-> list[tuple[str,str]]:
     return notes
     
 notes = read_obsidian_notes("notes")
+
 for name, content in notes:
     print(f"Name: {name}\n\nFirst 50 char of note:\n{content:50}...\n")
 
 
+def parseQA(content : str)-> list[tuple[str,str]]: 
     
+    lines = content.split("\n") # variable holding lines of notes
+
+    qaPairs = []
+    currentq = None
+    currenta = None
+
+    for line in lines:
+        line = line.strip()
+        
+        if line.startswith("Q:"):
+            #Save prev q/a if exists
+            if currentq and currenta:
+                qaPairs.append((currentq,currenta))
+                currenta = None
+            currentq = line[2:].strip()
+        
+        elif line.startswith("A:") and currentq:
+            currenta = line[2:].strip()
+            qaPairs.append((currentq,currenta))
+            currentq = None
+        
+        elif currentq and currenta is None:
+            #handle for multiline answer
+            currenta = line.strip()
+        
+        elif currenta and currentq:
+            #append to multiline answer
+            currenta += "\n" + line.strip()
+    
+    if currentq and currenta:
+        qaPairs.append((currentq,currenta))
+    
+    return qaPairs
+
+content = """
+Q: What is Python?
+A: A programming language.
+
+Q: What is Anki?
+A: A flashcard app.
+"""
+
+pairs = parseQA(content)   
+print(pairs)
+
+
+
     
 
 
---
+++ notes/sample_note2.md
@@ -0,0 +1,11 @@
+## Warra note 
+
+
+Q: Definition of Sigma
+A: This note
+
+some times there is stuff that doesnt matter
+
+A: but also important stuff
+
+
